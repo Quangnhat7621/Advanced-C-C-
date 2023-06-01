a92398df0b9df59e3511b70924053f318a873f12
Tran Quang Nhat <quangnhat310c@gmail.com>
2023-06-01 23:50:45 +0700

    fixing code  form

--- homeWork/sortingAndCount.c
+++ homeWork/sortingAndCount.c
@@ -9,16 +9,13 @@ void sort(uint8_t length, uint8_t _arr[])
     {
         for(uint8_t j = i + 1; j < length; j++)
         {
-
             if(_arr[i] > _arr[j])
             {
                 uint8_t temp;
                 temp = _arr[i];
                 _arr[i] = _arr[j];
                 _arr[j] = temp;
-                
             }
-            
         }
     }
 }
@@ -32,24 +29,18 @@ void frequency(uint8_t arr_len, uint8_t _arr[])
     uint8_t index = 0;
     for(uint8_t i = 0; i < arr_len - 1; i++)
     {
-
        if(_arr[i] == _arr[i+1])
        {
             if((i + 1) == (arr_len - 1))
             {
-
-                
                 count++;
                 number[index] = _arr[i];
                 freg[index] = count;
-
             }
             else
             {
                 count++;
             }
-            
-
        }
        else if( _arr[i] != _arr[i + 1])
        {
@@ -59,7 +50,6 @@ void frequency(uint8_t arr_len, uint8_t _arr[])
                 freg[index] = count;
                 index++;
                 count = 1;
-
             }
             else
             {
@@ -82,17 +72,23 @@ void frequency(uint8_t arr_len, uint8_t _arr[])
 
 void main()
 {
-
+// create an array
 uint8_t arr[] = {1, 5, 8, 6, 2, 5, 7, 5, 8, 1, 2, 6, 10, 12};
-uint8_t lengthOfArray = 14;
 
-sort(lengthOfArray, arr);
+// array length
+uint8_t lengthOfArray = 14; 
+
+// sorting array
+sort(lengthOfArray, arr);   
+
+// print array after sorting
 for(uint8_t i = 0; i < lengthOfArray; i++)
     {
         printf("%d \t", arr[i]);
-        
     }
 printf("\n");
+
+// count appeared frequencies
 frequency(lengthOfArray, arr);
 
 }
